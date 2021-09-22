c1397eafe33c7da6e0fe1ebcbb4c82f8d28c8f59
NGUYEN DAT <YootInter@2021>
2021-09-22 21:39:02 +0700

    NTDat -init project

---
+++ pages/test.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from "next";
+import { SiteLayout } from "../components/common";
+import Link from "next/link";
+
+const Test: NextPage = () => {
+  return (
+    <SiteLayout>
+      <div className="mt-8 max-w-xl mx-auto px-8">
+        <h1 className="text-center">
+          <span className="block text-xl text-gray-600 leading-tight">
+            Welcome to this
+          </span>
+          <span className="block text-5xl font-bold leading-none">
+            Awesome Website
+          </span>
+        </h1>
+        <div className="mt-12 text-center">
+          <Link href="/account-settings/basic-information">
+            <a className="inline-block bg-gray-900 hover:bg-gray-800 text-white font-medium rounded-lg px-6 py-4 leading-tight">
+              View account settings
+            </a>
+          </Link>
+        </div>
+      </div>
+    </SiteLayout>
+  );
+};
+
+export default Test;
